9cf4d14d5049e75477f34fec90f6a25920949f9d
francisamidu <francisamidu124@gmail.com>
2022-12-30 09:09:14 +0200

    Updated README.md

--- helpers/uid.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var uuid_1 = require("uuid");
-var uid = function () {
-    var id = (0, uuid_1.v4)();
-    return id;
-};
-exports["default"] = uid;
--- pages/_app.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-exports.__esModule = true;
-require("../styles/globals.scss");
-function MyApp(_a) {
-    var Component = _a.Component, pageProps = _a.pageProps;
-    return <Component {...pageProps}/>;
-}
-exports["default"] = MyApp;
