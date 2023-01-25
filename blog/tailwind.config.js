ac5106aeb9096422cf5b716a7f038361fde7f373
francisamidu <francisamidu124@gmail.com>
2023-01-25 18:21:14 +0200

    Added tailwindcss config files

---
+++ blog/tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['.src/components/**/*.tsx', '.src/pages/**/*.tsx'],
+  theme: {
+    extend: {},
+  },
+  plugins: [require('@tailwindcss/typography')],
+  purge: [],
+}
